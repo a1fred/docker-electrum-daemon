f8ff19843e0c6d54a14019568fde366e1be985f6
Dmirty Simonov <demalf@gmail.com>
2019-11-13 18:36:51 +0300

    Wallet automatic load from seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,29 @@ electrum setconfig rpcpassword ${ELECTRUM_PASSWORD}
 electrum setconfig rpchost 0.0.0.0
 electrum setconfig rpcport 7000
 
-# XXX: Check load wallet or create
-
 # Run application
 electrum daemon start
+
+# Check load wallet or create
+if [ ! -z "$ELECTRUM_SEED_PHRASE" ]
+then
+	echo "Seed defined. Perform wallet operations"
+
+  if [ ! -e "/home/electrum/.electrum/wallets/default_wallet" ]
+  then
+    echo "default_wallet not exists. Creating from given seed."
+    electrum restore --passphrase= "${ELECTRUM_SEED_PHRASE}"
+  fi
+else
+	echo "Seed not defined. Skipping wallet creation"
+fi
+
+# Loading wallet if exists
+if [ -e "/home/electrum/.electrum/wallets/default_wallet" ]
+then
+  echo "Loading wallet."
+  electrum daemon load_wallet
+fi
 
 # Wait forever
 while true; do
